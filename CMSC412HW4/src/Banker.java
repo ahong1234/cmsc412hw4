6259e970c2ad8b0c924c7f842cf1b88156702827
alex <alexhong240@gmail.com>
2023-02-07 20:39:16 -0500

    2nd

--- CMSC412HW4/src/Banker.java
+++ CMSC412HW4/src/Banker.java
@@ -5,32 +5,37 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class readInput {
-	private String fileName;
-	private String[] processAndResource;
-	private String[] totalAndAvailable;
-	private String[] maxAndAlloc;
-	private int[] total;
-	private int[] available;
-	private int processes, resources;
-	private int[][] max;
-	private int[][] allocated;
-	private int[][] need;
-	private int[] allocTotal;
+public class Banker {
+	public String fileName;
+	public String[] processAndResource;
+	public String[] totalAndAvailable;
+	public String[] maxAndAlloc;
+	public int[] total;
+	public int[] available;
+	public int processes, resources;
+	public int[][] max;
+	public int[][] allocated;
+	public int[][] need;
+	public int[] allocTotal;
+	public int[] work;
+	public boolean[] finish;
+	public int[] seq;
 	
-	public readInput() {
+	
+	public Banker() {
 		
 		//Scanner stdin = new Scanner(System.in);
 //		System.out.println("enter file name: ");
 //		fileName = stdin.next();
 		
 //		try (BufferedReader br = new BufferedReader(new FileReader("src/" + fileName))) {
-		try (BufferedReader br = new BufferedReader(new FileReader("src/input1.txt"))) {
+		try (BufferedReader br = new BufferedReader(new FileReader("src/input3.txt"))) {
 			int lineNumber = 0;
 			boolean scanning = true;
 			while (scanning) {
 			switch (lineNumber) {
 				case 0:
+					// scan first line for N and M
 					scanNM(br);
 					lineNumber++;
 					total = new int[resources];
@@ -42,11 +47,26 @@ public class readInput {
 					System.out.println("processes: "+ processes + " resources: " + resources);
 					break;
 				case 1:
+					// scan second line for total available resources
 					scanTotAvail(br);
 					lineNumber++;
 					break;
 				default:
+					// scan max and allocated matrices
 					scanMatrices(br);
+					// calculate need and available matrices
+					calculateNeed(max, allocated);
+					sumColumns(allocated);
+					calculateAvailable(total, allocTotal);
+					printMatrice(max, allocated);
+					if(findSequence(available, allocated)) {
+						System.out.println(Arrays.toString(work));
+						System.out.println("Sequence found: "+ Arrays.toString(seq));
+					}
+					else {
+						System.out.println("no sequence found");
+					}
+					
 					scanning = false;
 			}
 			}
@@ -95,11 +115,12 @@ public class readInput {
 	
 	public void scanTotAvail(BufferedReader br) {
 		try {
-			int count = 0;
+			
 			totalAndAvailable = br.readLine().split("\\s+");
+			
 			for(int i = 0; i < resources; i++) {
 				total[i] = Integer.parseInt(totalAndAvailable[i]);
-				count++;
+				
 			}
 			
 			System.out.println("total resources: " + Arrays.toString(total));
@@ -139,9 +160,8 @@ public class readInput {
 				}
 				count = 0;
 			}
-			System.out.println(Arrays.deepToString(max) + " " + Arrays.deepToString(allocated));
-			calculateNeed(max, allocated);
-			sumColumns(allocated);
+
+			
 		}
 		catch (IOException e) {
 			System.out.println("error: IOException");
@@ -174,7 +194,7 @@ public class readInput {
 				}
 			}
 		}
-		System.out.println(Arrays.deepToString(need));
+	
 	}
 	
 	public void sumColumns(int[][] alloc) {
@@ -183,9 +203,79 @@ public class readInput {
 				allocTotal[j] += alloc[i][j];
 			}
 		}
-		System.out.println(Arrays.toString(allocTotal));
+	
 	}
-	public void calculateTotal(int[][] available, int[][] alloc) {
+	
+	public void calculateAvailable(int[] total, int[] alloc) {
+		for(int i = 0; i < resources; i++) {
+			available[i] = total[i] - alloc[i];
+		}
+	
+	}
+	public void printMatrice(int[][] max, int[][] alloc) {
+		for(int i = 0; i < processes; i++) {
+			String line = "";
+			for(int j = 0; j < resources; j++) {
+				line += max[i][j] + " ";
+			}
+			line += " ";
+			for(int j = 0; j < resources; j++) {
+				line += alloc[i][j] + " ";
+			}
+			System.out.println(line);
+		}
+	}
+	
+	public boolean findSequence(int[] available, int[][] allocation) {
+		seq = new int[processes];
+		boolean found = false;
+		boolean processGood = false;
+		work = available;
+		finish = new boolean[processes];
+		for (int i = 0; i < processes; i++) {
+			finish[i] = false;
+		}
+		int count = 0;
+			while (count < processes) {
+				
+			// scan processes
+				
+				for (int i = 0; i < processes; i++) {
+					System.out.println(i);
+					if (!finish[i]) {
+						int j = 0;
+						for (; j < resources; j++) {
+							if (need[i][j] > work[j]) {
+								System.out.println("process: " + i);
+								break;
+							}
+							if ((j+1) == resources) {
+								
+								for (int k = 0; k < resources; k++) {
+						              work[k] = work[k] + allocation[i][k];
+						              
+						        }
+								System.out.println(Arrays.toString(work));
+								seq[count++] = (i);
+								finish[i] = true;
+								found = true;
+								
+								
+							}
+					 }
+						
+				   }
+					
+				}
+				if (!found) {		
+					return false;
+				}
+			}
+			return true;	
+	}
+	
+	public static void main(String[] args) throws IOException {
+		Banker bk = new Banker();
 		
 	}
 }
--- CMSC412HW4/src/Main.java
+++ CMSC412HW4/src/Main.java
@@ -2,10 +2,6 @@ import java.io.IOException;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		readInput rd = new readInput();
-		
-	}
+	
 
 }
